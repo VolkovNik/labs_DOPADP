a58a56c11809f6a96bad44e45f8612c05ac4b239
Nikita Volkov <nikitavolkov1707@gmail.com>
2020-10-16 22:34:43 +0300

    Scripted auto-commit on change (2020-10-16 22:34:43) by gitwatch.sh

--- src/main/java/ru/bmstu/airport/flight/join/AirportFlightJoin.java
+++ src/main/java/ru/bmstu/airport/flight/join/AirportFlightJoin.java
@@ -2,7 +2,9 @@ package ru.bmstu.airport.flight.join;
 
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.MultipleInputs;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 public class AirportFlightJoin {
     public static void main(String[] args) {
