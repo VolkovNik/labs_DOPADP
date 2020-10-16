e73395368809c604a4debe471a4b9bb67ee45718
Nikita Volkov <nikitavolkov1707@gmail.com>
2020-10-16 22:33:23 +0300

    Scripted auto-commit on change (2020-10-16 22:33:23) by gitwatch.sh

--- src/main/java/ru/bmstu/airport/flight/join/AirportFlightJoin.java
+++ src/main/java/ru/bmstu/airport/flight/join/AirportFlightJoin.java
@@ -1,4 +1,24 @@
 package ru.bmstu.airport.flight.join;
 
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.mapred.lib.MultipleInputs;
+import org.apache.hadoop.mapreduce.Job;
+
 public class AirportFlightJoin {
+    public static void main(String[] args) {
+        Job job = Job.getInstance();
+        job.setJarByClass(AirportFlightJoin.class);
+        job.setJobName("JoinJob sort");
+        MultipleInputs.addInputPath(job, new Path(args[0]), TextInputFormat.class, CallsJoinMapper.class);
+        MultipleInputs.addInputPath(job, new Path(args[1]), TextInputFormat.class, SystemsJoinMapper.class);
+        FileOutputFormat.setOutputPath(job, new Path(args[2]));
+        job.setPartitionerClass(TextPair.FirstPartitioner.class);
+        job.setGroupingComparatorClass(TextPair.FirstComparator.class);
+        job.setReducerClass(JoinReducer.class);
+        job.setMapOutputKeyClass(TextPair.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(Text.class);
+        job.setNumReduceTasks(2);
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
+    }
 }
