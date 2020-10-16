7f6fb0f2605198fb3b6521ad1ecd33c440ecd55e
Nikita Volkov <nikitavolkov1707@gmail.com>
2020-10-16 22:41:17 +0300

    Scripted auto-commit on change (2020-10-16 22:41:17) by gitwatch.sh

--- src/main/java/ru/bmstu/airport/flight/join/AirportWritableComparable.java
+++ src/main/java/ru/bmstu/airport/flight/join/AirportWritableComparable.java
@@ -2,5 +2,30 @@ package ru.bmstu.airport.flight.join;
 
 import org.apache.hadoop.io.WritableComparable;
 
-public class AirportWritableComparable extends WritableComparable<> {
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+
+public class AirportWritableComparable implements WritableComparable {
+    int airportID;
+    int indicator;
+
+    void setAirportID(int newAirportId) {
+
+    }
+
+    @Override
+    public int compareTo(Object o) {
+        return 0;
+    }
+
+    @Override
+    public void write(DataOutput dataOutput) throws IOException {
+
+    }
+
+    @Override
+    public void readFields(DataInput dataInput) throws IOException {
+
+    }
 }
