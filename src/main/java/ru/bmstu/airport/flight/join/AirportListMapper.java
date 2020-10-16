8060ddcac3931877dff42b776b7d40051494823e
Nikita Volkov <nikitavolkov1707@gmail.com>
2020-10-16 23:09:52 +0300

    Scripted auto-commit on change (2020-10-16 23:09:52) by gitwatch.sh

--- src/main/java/ru/bmstu/airport/flight/join/AirportListMapper.java
+++ src/main/java/ru/bmstu/airport/flight/join/AirportListMapper.java
@@ -9,6 +9,8 @@ import java.io.IOException;
 public class AirportListMapper extends Mapper<LongWritable, Text, AirportWritableComparable, Text> {
     @Override
     protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
-        super.map(key, value, context);
+        String aiportList = value.toString();
+        String[] airportValues = aiportList.split(",");
+        if (airportValues[0] != )
     }
 }
