ca985cd17317ca07dcd30e0ee3a91bec78c1e80d
Nikita Volkov <nikitavolkov1707@gmail.com>
2020-10-17 11:38:20 +0300

    Scripted auto-commit on change (2020-10-17 11:38:20) by gitwatch.sh

--- src/main/java/ru/bmstu/airport/flight/join/FlightListMapper.java
+++ src/main/java/ru/bmstu/airport/flight/join/FlightListMapper.java
@@ -11,12 +11,15 @@ public class FlightListMapper extends Mapper<LongWritable, Text, AirportWritable
     protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
         String flightList = value.toString();
         String[] flightValues = flightList.split(",");
-        if (!flightValues[0].equals("\"YEAR\"")) {
-            AirportWritableComparable keyFlightList = new AirportWritableComparable();
-            int airportID = Integer.parseInt(flightValues[14]);
-            keyFlightList.setAirportID(airportID);
-            keyFlightList.setIndicator(1);
-            context.write(keyFlightList, new Text(flightValues[0]));
+        if (!flightValues[0].equals("\"YEAR\"") && flightValues[20].equals("")) {
+            if (0 < Float.parseFloat(flightValues[18])) {
+                AirportWritableComparable keyFlightList = new AirportWritableComparable();
+                int airportID = Integer.parseInt(flightValues[14]);
+                float
+                keyFlightList.setAirportID(airportID);
+                keyFlightList.setIndicator(1);
+                context.write(keyFlightList, new Text(flightValues[0]));
+            }
         }
 //        AirportWritableComparable keyFromAirport = new AirportWritableComparable();
 //        keyFromAirport.setIndicator(1);
