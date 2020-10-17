c28bc60bbecf4fc49a8f35083278627bf3ac9977
Nikita Volkov <nikitavolkov1707@gmail.com>
2020-10-17 12:01:58 +0300

    Scripted auto-commit on change (2020-10-17 12:01:58) by gitwatch.sh

--- src/main/java/ru/bmstu/airport/flight/join/FlightListMapper.java
+++ src/main/java/ru/bmstu/airport/flight/join/FlightListMapper.java
@@ -12,20 +12,14 @@ public class FlightListMapper extends Mapper<LongWritable, Text, AirportWritable
         String flightList = value.toString();
         String[] flightValues = flightList.split(",");
         float codeDelay = Float.parseFloat(flightValues[19]);
-        //flightValues[20] = flightValues[20].replaceAll("^\"+|\"+$", "");
         if (!flightValues[0].equals("\"YEAR\"") && codeDelay == 0) {
             if (0 < Float.parseFloat(flightValues[18])) {
                 AirportWritableComparable keyFlightList = new AirportWritableComparable();
                 int airportID = Integer.parseInt(flightValues[14]);
-                //float delay = Float.parseFloat(flightValues[20]);
                 keyFlightList.setAirportID(airportID);
                 keyFlightList.setIndicator(1);
                 context.write(keyFlightList, new Text(flightValues[18]));
             }
         }
-//        AirportWritableComparable keyFromAirport = new AirportWritableComparable();
-//        keyFromAirport.setIndicator(1);
-//        keyFromAirport.setAirportID(228);
-//        context.write(keyFromAirport, new Text(flightValues[1]));
     }
 }
