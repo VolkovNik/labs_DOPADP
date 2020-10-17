f22f4d603b6487c9747c387b2f6498cedbc93485
Nikita Volkov <nikitavolkov1707@gmail.com>
2020-10-17 12:39:27 +0300

    Scripted auto-commit on change (2020-10-17 12:39:27) by gitwatch.sh

--- src/main/java/ru/bmstu/airport/flight/join/AirportFlightReducer.java
+++ src/main/java/ru/bmstu/airport/flight/join/AirportFlightReducer.java
@@ -9,6 +9,10 @@ import java.io.IOException;
 import java.util.Iterator;
 
 public class AirportFlightReducer extends Reducer<AirportWritableComparable, Text, Text, Text> {
+    public float getDelay(String delay) {
+        return Float.parseFloat(delay);
+    }
+
     @Override
     protected void reduce(AirportWritableComparable key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
         String airportName = "Airport: ";
@@ -16,15 +20,23 @@ public class AirportFlightReducer extends Reducer<AirportWritableComparable, Tex
         Iterator<Text> val_iterator = values.iterator();
         airportName += val_iterator.next().toString();
         if (val_iterator.hasNext()) {
-            float minDelay = Float.parseFloat(val_iterator.next().toString());
-            float maxDelay = Float.parseFloat(val_iterator.next().toString());
-            float sum = Float.parseFloat(val_iterator.next().toString());
+            float minDelay = getDelay(val_iterator.next().toString());
+            float maxDelay = getDelay(val_iterator.next().toString());
+            float sum = getDelay(val_iterator.next().toString());
             int counter = 1;
             while (val_iterator.hasNext()) {
-                Text delay = val_iterator.next();
-                delays += delay.toString() + " ";
+                float delay = getDelay(val_iterator.next().toString());
+                if (delay < minDelay) {
+                    minDelay = delay;
+                }
+                if (delay > maxDelay) {
+                    maxDelay = delay;
+                }
+                sum += delay;
+                counter++;
             }
-            context.write(new Text(airportName), new Text(delays));
+
         }
+        context.write(new Text(airportName), new Text(delays));
     }
 }
