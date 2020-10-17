d0313a7375a9039717b2cbec8241609b6f15ad46
Nikita Volkov <nikitavolkov1707@gmail.com>
2020-10-17 10:34:00 +0300

    Scripted auto-commit on change (2020-10-17 10:34:00) by gitwatch.sh

--- src/main/java/ru/bmstu/airport/flight/join/AirportFlightGroupingComparator.java
+++ src/main/java/ru/bmstu/airport/flight/join/AirportFlightGroupingComparator.java
@@ -4,6 +4,10 @@ import org.apache.hadoop.io.WritableComparable;
 import org.apache.hadoop.io.WritableComparator;
 
 public class AirportFlightGroupingComparator extends WritableComparator {
+    public AirportWritableComparable() {
+        super(.class, true);
+    }
+
     @Override
     public int compare(WritableComparable a, WritableComparable b) {
         return super.compare(a, b);
