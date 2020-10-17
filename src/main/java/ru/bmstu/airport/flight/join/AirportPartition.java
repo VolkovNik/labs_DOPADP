d268196a4327d851bc006ec63882766910eaad65
Nikita Volkov <nikitavolkov1707@gmail.com>
2020-10-17 17:22:35 +0300

    Scripted auto-commit on change (2020-10-17 17:22:35) by gitwatch.sh

--- src/main/java/ru/bmstu/airport/flight/join/AirportPartition.java
+++ src/main/java/ru/bmstu/airport/flight/join/AirportPartition.java
@@ -7,5 +7,6 @@ public class AirportPartition extends Partitioner<AirportWritableComparable, Tex
     @Override
     public int getPartition(AirportWritableComparable airportWritableComparable, Text text, int i) {
         return airportWritableComparable.getAirportID() % i;
+        
     }
 }
