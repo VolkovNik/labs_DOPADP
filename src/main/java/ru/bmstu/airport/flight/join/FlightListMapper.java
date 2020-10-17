c3a829cfbd2879c87e4fc9589d78b972fae9cc1d
Nikita Volkov <nikitavolkov1707@gmail.com>
2020-10-17 16:16:51 +0300

    Scripted auto-commit on change (2020-10-17 16:16:50) by gitwatch.sh

--- src/main/java/ru/bmstu/airport/flight/join/FlightListMapper.java
+++ src/main/java/ru/bmstu/airport/flight/join/FlightListMapper.java
@@ -10,8 +10,11 @@ public class FlightListMapper extends Mapper<LongWritable, Text, AirportWritable
     private static final String REGEX_SPLITTER_CVS = ",";
     private static final String FLAG_FIRST_STRING = "\"YEAR\"";
     private static final int ZERO_COLUMN = 0;
+    private static final int ID_AIRPORT_COLUMN = 14;
     private static final int CODE_DELAY_COLUMN = 19;
+    private static final int ARR_DELAY_COLUMN = 18;
     private static final String EMPTY_STRING = "";
+    private static final int INDICATOR = 1;
 
     private String[] getArrayOfValues(String strValue) {
         return strValue.split(REGEX_SPLITTER_CVS);
@@ -22,8 +25,10 @@ public class FlightListMapper extends Mapper<LongWritable, Text, AirportWritable
     }
 
     private float getCodeDelay(String strCode) {
-        return Float.parseFloat(strCode)
+        return Float.parseFloat(strCode);
     }
+
+
 
     @Override
     protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
@@ -32,15 +37,14 @@ public class FlightListMapper extends Mapper<LongWritable, Text, AirportWritable
 
         if (!isFirstString(flightValues[ZERO_COLUMN])) {
             float codeDelay = getCodeDelay(flightValues[CODE_DELAY_COLUMN]);
-            //float codeDelay = Float.parseFloat(flightValues[19]);
 
-            if (codeDelay == 0 && !flightValues[18].equals(EMPTY_STRING)) {
-                if ((float)0 < Float.parseFloat(flightValues[18])) {
+            if (codeDelay == 0 && !flightValues[ARR_DELAY_COLUMN].equals(EMPTY_STRING)) {
+                if ((float)0 < Float.parseFloat(flightValues[ARR_DELAY_COLUMN])) {
                     AirportWritableComparable keyFlightList = new AirportWritableComparable();
-                    int airportID = Integer.parseInt(flightValues[14]);
+                    int airportID = Integer.parseInt(flightValues[ID_AIRPORT_COLUMN]);
                     keyFlightList.setAirportID(airportID);
-                    keyFlightList.setIndicator(1);
-                    context.write(keyFlightList, new Text(flightValues[18]));
+                    keyFlightList.setIndicator(INDICATOR);
+                    context.write(keyFlightList, new Text(flightValues[ARR_DELAY_COLUMN]));
                 }
             }
         }
