8a4a8f2124177fdbaf7a26afc243ef50e392df05
Nikita Volkov <nikitavolkov1707@gmail.com>
2020-11-13 17:45:58 +0300

    Scripted auto-commit on change (2020-11-13 17:45:58) by gitwatch.sh

--- lab3/src/main/java/ru/bmstu/airport/pair/find/delay/AiportFindDelayApp.java
+++ lab3/src/main/java/ru/bmstu/airport/pair/find/delay/AiportFindDelayApp.java
@@ -6,15 +6,21 @@ import org.apache.spark.api.java.JavaSparkContext;
 
 public class AiportFindDelayApp {
     private static final String REGEX_SPLITTER_CVS = ",";
-    private static final String FLAG_FIRST_STRING = "\"YEAR\"";
+    //private static final String FLAG_FIRST_STRING = "\"YEAR\"";
     private static final int ZERO_COLUMN = 0;
-    private static final int ID_AIRPORT_COLUMN = 14;
+    //private static final int ID_AIRPORT_COLUMN = 14;
     private static final int CANCELLED_COLUMN = 19;
     private static final int ARR_DELAY_COLUMN = 18;
     private static final String EMPTY_STRING = "";
     private static final float CANCEL_CODE = 1;
     private static final float ZERO_TIME = 0;
     private static final int INDICATOR_FLIGHT_MAPPER = 1;
+    private static final String REGEX_FOR_QUOTES = "^\"+|\"+$";
+    private static final String REPLACEMENT_TO_NULL_STR = "";
+    private static final int ID_AIRPORT_COLUMN = 0;
+    private static final int NAME_AIRPORT_COLUMN = 1;
+    private static final int INDICATOR_AIRPORT_MAPPER = 0;
+    private static final String FLAG_FIRST_STRING = "Code";
 
     public static void main(String[] args) {
         SparkConf conf = new SparkConf().setAppName("lab3");
@@ -25,7 +31,7 @@ public class AiportFindDelayApp {
 
         JavaRDD<String> test = airportsTable.filter(string -> !string.contains("Code")).
                 map(string -> {
-                    
+
                     return string;
                 });
 
