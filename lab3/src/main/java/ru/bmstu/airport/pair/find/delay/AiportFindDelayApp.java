7a798406d0451bc070b89bea66ccad4ff0abdeea
Nikita Volkov <nikitavolkov1707@gmail.com>
2020-11-13 16:25:47 +0300

    Scripted auto-commit on change (2020-11-13 16:25:47) by gitwatch.sh

--- lab3/src/main/java/ru/bmstu/airport/pair/find/delay/AiportFindDelayApp.java
+++ lab3/src/main/java/ru/bmstu/airport/pair/find/delay/AiportFindDelayApp.java
@@ -5,6 +5,17 @@ import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 
 public class AiportFindDelayApp {
+    private static final String REGEX_SPLITTER_CVS = ",";
+    private static final String FLAG_FIRST_STRING = "\"YEAR\"";
+    private static final int ZERO_COLUMN = 0;
+    private static final int ID_AIRPORT_COLUMN = 14;
+    private static final int CANCELLED_COLUMN = 19;
+    private static final int ARR_DELAY_COLUMN = 18;
+    private static final String EMPTY_STRING = "";
+    private static final float CANCEL_CODE = 1;
+    private static final float ZERO_TIME = 0;
+    private static final int INDICATOR_FLIGHT_MAPPER = 1;
+
     public static void main(String[] args) {
         SparkConf conf = new SparkConf().setAppName("lab3");
         JavaSparkContext sc = new JavaSparkContext(conf);
@@ -12,6 +23,9 @@ public class AiportFindDelayApp {
         JavaRDD<String> airportsTable = sc.textFile("AirportList.csv");
         JavaRDD<String> flightTable = sc.textFile("FlightList.csv");
 
-        
+        JavaRDD<String> flightString = flightTable.
+
+
+
     }
 }
