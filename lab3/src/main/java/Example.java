67403b65ec449644414d676a1c160a8904667aa8
Nikita Volkov <nikitavolkov1707@gmail.com>
2020-11-13 13:19:14 +0300

    Scripted auto-commit on change (2020-11-13 13:19:14) by gitwatch.sh

--- lab3/src/main/java/Example.java
+++ lab3/src/main/java/Example.java
@@ -1,2 +1,7 @@
+import org.apache.spark.SparkConf;
+
 public class Example {
+    public static void main(String[] args) {
+        SparkConf conf = new SparkConf(Hadoop ).setAppName(Hadoop "example");
+    }
 }
