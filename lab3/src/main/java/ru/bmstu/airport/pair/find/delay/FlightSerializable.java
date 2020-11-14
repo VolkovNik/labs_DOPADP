e1c5d4e2665ca3980324dfb3e749bfcd9b0c3eec
Nikita Volkov <nikitavolkov1707@gmail.com>
2020-11-14 14:36:37 +0300

    Scripted auto-commit on change (2020-11-14 14:36:37) by gitwatch.sh

--- lab3/src/main/java/ru/bmstu/airport/pair/find/delay/FlightSerializable.java
+++ lab3/src/main/java/ru/bmstu/airport/pair/find/delay/FlightSerializable.java
@@ -3,8 +3,8 @@ package ru.bmstu.airport.pair.find.delay;
 import java.io.Serializable;
 
 public class FlightSerializable implements Serializable {
-    private boolean cancelFlag;
-    private float delayTime;
+    private final boolean cancelFlag;
+    private final float delayTime;
 
     FlightSerializable(boolean cancelFlag, float delayTime) {
         this.cancelFlag = cancelFlag;
