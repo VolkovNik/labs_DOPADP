8e9237d40fb30e289bce894bc999c40187f87099
Nikita Volkov <nikitavolkov1707@gmail.com>
2020-11-14 14:40:28 +0300

    Scripted auto-commit on change (2020-11-14 14:40:28) by gitwatch.sh

--- lab3/src/main/java/ru/bmstu/airport/pair/find/delay/AiportFindDelayApp.java
+++ lab3/src/main/java/ru/bmstu/airport/pair/find/delay/AiportFindDelayApp.java
@@ -59,9 +59,7 @@ public class AiportFindDelayApp {
         } else
             return ZERO_TIME;
     }
-
-
-
+    
     public static void main(String[] args) {
         SparkConf conf = new SparkConf().setAppName("lab3");
         JavaSparkContext sc = new JavaSparkContext(conf);
