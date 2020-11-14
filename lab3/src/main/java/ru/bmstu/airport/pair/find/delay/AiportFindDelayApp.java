06fc2e40c5d2d05a09888d811daa0c138b327127
Nikita Volkov <nikitavolkov1707@gmail.com>
2020-11-14 15:34:07 +0300

    Scripted auto-commit on change (2020-11-14 15:34:07) by gitwatch.sh

--- lab3/src/main/java/ru/bmstu/airport/pair/find/delay/AiportFindDelayApp.java
+++ lab3/src/main/java/ru/bmstu/airport/pair/find/delay/AiportFindDelayApp.java
@@ -170,7 +170,7 @@ public class AiportFindDelayApp {
         JavaPairRDD<Tuple2<Integer, Integer>, TestingCombine> combine =
                 flightInformation.combineByKey(
                         p -> new Testing(p.getDelayTime(), 1),
-                        (combine, )
+                        (combine, p) -> 
                 )
 
         test.saveAsTextFile("output");
