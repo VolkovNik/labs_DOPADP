dcc14c43574096516e8bd775f928813e47986368
Nikita Volkov <nikitavolkov1707@gmail.com>
2020-11-15 17:17:03 +0300

    Scripted auto-commit on change (2020-11-15 17:17:03) by gitwatch.sh

--- lab3/src/main/java/ru/bmstu/airport/pair/find/delay/AiportFindDelayApp.java
+++ lab3/src/main/java/ru/bmstu/airport/pair/find/delay/AiportFindDelayApp.java
@@ -203,6 +203,7 @@ public class AiportFindDelayApp {
 
         JavaRDD<String> ans = flightDataCombined.map(
                 value -> {
+                    Map<Integer, String> airportName = airportBroadcasted.getValue();
                     
                 }
         )
