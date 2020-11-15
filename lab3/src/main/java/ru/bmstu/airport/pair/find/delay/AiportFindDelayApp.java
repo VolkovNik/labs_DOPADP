dd7f20be6df207167b6bc0afb74b3e6840876393
Nikita Volkov <nikitavolkov1707@gmail.com>
2020-11-15 16:43:20 +0300

    Scripted auto-commit on change (2020-11-15 16:43:20) by gitwatch.sh

--- lab3/src/main/java/ru/bmstu/airport/pair/find/delay/AiportFindDelayApp.java
+++ lab3/src/main/java/ru/bmstu/airport/pair/find/delay/AiportFindDelayApp.java
@@ -190,7 +190,8 @@ public class AiportFindDelayApp {
                                 value.getDelayTime() > ZERO_TIME ? 1 : 0
                         ),
                         
-                )
+
+                );
 
 
 
