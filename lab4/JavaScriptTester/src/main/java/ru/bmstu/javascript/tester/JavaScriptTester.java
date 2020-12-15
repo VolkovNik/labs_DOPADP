b21224cd961aa683c7104f0939750332d366cc80
Nikita Volkov <nikitavolkov1707@gmail.com>
2020-12-15 14:06:10 +0300

    Scripted auto-commit on change (2020-12-15 14:06:10) by gitwatch.sh

--- lab4/JavaScriptTester/src/main/java/ru/bmstu/javascript/tester/JavaScriptTester.java
+++ lab4/JavaScriptTester/src/main/java/ru/bmstu/javascript/tester/JavaScriptTester.java
@@ -8,11 +8,13 @@ import javax.script.ScriptException;
 public class JavaScriptTester {
     public static void main(String[] args) throws ScriptException, NoSuchMethodException {
 
-        ScriptEngine engine = new ScriptEngineManager().getEngineByName("nashorn");
-        engine.eval("var divideFn = function(a,b) { return a/b}");
-        Invocable invocable = (Invocable) engine;
-        Object[] params = {2, 1};
-        System.out.println(invocable.invokeFunction("divideFn", params).toString());
+//        ScriptEngine engine = new ScriptEngineManager().getEngineByName("nashorn");
+//        engine.eval("var divideFn = function(a,b) { return a/b}");
+//        Invocable invocable = (Invocable) engine;
+//        Object[] params = {2, 1};
+//        System.out.println(invocable.invokeFunction("divideFn", params).toString());
+
+        
 
     }
 }
