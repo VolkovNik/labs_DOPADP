72612840567441d2f3a463ca6a507e3f670104d2
Nikita Volkov <nikitavolkov1707@gmail.com>
2020-12-15 20:31:37 +0300

    Scripted auto-commit on change (2020-12-15 20:31:37) by gitwatch.sh

--- lab4/JavaScriptTester/src/main/java/ru/bmstu/javascript/tester/ActorExecutor.java
+++ lab4/JavaScriptTester/src/main/java/ru/bmstu/javascript/tester/ActorExecutor.java
@@ -3,15 +3,22 @@ package ru.bmstu.javascript.tester;
 import akka.actor.AbstractActor;
 
 public class ActorExecutor extends AbstractActor {
-    @Override
-    public Receive createReceive() {
-        return null;
-    }
 
+    public String executeTest {
+        
+    }
     //        ScriptEngine engine = new ScriptEngineManager().getEngineByName("nashorn");
 //        engine.eval("var divideFn = function(a,b) { return a/b}");
 //        Invocable invocable = (Invocable) engine;
 //        Object[] params = {2, 1};
 //        System.out.println(invocable.invokeFunction("divideFn", params).toString());
+
+
+    @Override
+    public Receive createReceive() {
+        return null;
+    }
+
+
 
 }
