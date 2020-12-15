7e97002d3d1aa2fc4d142fd0f300c4304ab12a08
Nikita Volkov <nikitavolkov1707@gmail.com>
2020-12-15 16:35:36 +0300

    Scripted auto-commit on change (2020-12-15 16:35:36) by gitwatch.sh

--- lab4/JavaScriptTester/src/main/java/ru/bmstu/javascript/tester/JavaScriptTester.java
+++ lab4/JavaScriptTester/src/main/java/ru/bmstu/javascript/tester/JavaScriptTester.java
@@ -1,7 +1,9 @@
 package ru.bmstu.javascript.tester;
 
 import akka.NotUsed;
+import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
+import akka.actor.Props;
 import akka.http.javadsl.ConnectHttp;
 import akka.http.javadsl.Http;
 import akka.http.javadsl.ServerBinding;
@@ -10,6 +12,7 @@ import akka.http.javadsl.model.HttpRequest;
 import akka.http.javadsl.model.HttpResponse;
 import akka.http.javadsl.server.AllDirectives;
 import akka.http.javadsl.server.Route;
+import akka.routing.RoundRobinPool;
 import akka.stream.ActorMaterializer;
 import akka.stream.javadsl.Flow;
 
@@ -25,10 +28,10 @@ public class JavaScriptTester extends AllDirectives {
 
         ActorSystem system = ActorSystem.create("routes");
         final Http http = Http.get(system);
-        ActorRouter router  = getContext().actorOf(
+        ActorRef router  = getContext().actorOf(
                 new RoundRobinPool(5)
                         .withSupervisorStrategy(strategy)
-                        .props(Props.create(ActorRouter.class, logResultsActor)),
+                        .props(Props.create(ActorRouter.class),
                 "routerForTests"
         final ActorMaterializer materializer = ActorMaterializer.create(system);
         JavaScriptTester instance = new JavaScriptTester();
