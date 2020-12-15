3125d1327f918dcbfea9702554e14a178eb335c1
Nikita Volkov <nikitavolkov1707@gmail.com>
2020-12-15 20:34:29 +0300

    Scripted auto-commit on change (2020-12-15 20:34:29) by gitwatch.sh

--- lab4/JavaScriptTester/src/main/java/ru/bmstu/javascript/tester/ActorExecutor.java
+++ lab4/JavaScriptTester/src/main/java/ru/bmstu/javascript/tester/ActorExecutor.java
@@ -2,23 +2,23 @@ package ru.bmstu.javascript.tester;
 
 import akka.actor.AbstractActor;
 
+import javax.script.Invocable;
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
+
 public class ActorExecutor extends AbstractActor {
 
-    public String executeTest {
-        
+    public String executeTest(RequestBody requestBody) throws ScriptException, NoSuchMethodException {
+        ScriptEngine engine = new ScriptEngineManager().getEngineByName("nashorn");
+        engine.eval("var divideFn = function(a,b) { return a/b}");
+        Invocable invocable = (Invocable) engine;
+        Object[] params = {2, 1};
+        return (invocable.invokeFunction("divideFn", params).toString());
     }
-    //        ScriptEngine engine = new ScriptEngineManager().getEngineByName("nashorn");
-//        engine.eval("var divideFn = function(a,b) { return a/b}");
-//        Invocable invocable = (Invocable) engine;
-//        Object[] params = {2, 1};
-//        System.out.println(invocable.invokeFunction("divideFn", params).toString());
-
 
     @Override
     public Receive createReceive() {
         return null;
     }
-
-
-
 }
