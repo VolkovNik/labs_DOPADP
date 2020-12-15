c1ab934684fa341ae9e9f679449cc6af08bb2d72
Nikita Volkov <nikitavolkov1707@gmail.com>
2020-12-15 13:47:13 +0300

    Scripted auto-commit on change (2020-12-15 13:47:13) by gitwatch.sh

--- lab4/JavaScriptTester/src/main/java/ru/bmstu/javascript/tester/JavaScriptTester.java
+++ lab4/JavaScriptTester/src/main/java/ru/bmstu/javascript/tester/JavaScriptTester.java
@@ -1,4 +1,10 @@
 package ru.bmstu.javascript.tester;
 
 public class JavaScriptTester {
+
+    ScriptEngine engine = new ScriptEngineManager().getEngineByName("nashorn");
+    engine.eval(jscript);
+    Invocable invocable = (Invocable) engine;
+    return invocable.invokeFunction(functionName, params).toString();
+
 }
