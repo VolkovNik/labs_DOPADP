eb4c9a46500d36e90eb37b09075e807cb5db9307
Nikita Volkov <nikitavolkov1707@gmail.com>
2020-12-15 19:38:39 +0300

    Scripted auto-commit on change (2020-12-15 19:38:39) by gitwatch.sh

--- lab4/JavaScriptTester/src/main/java/ru/bmstu/javascript/tester/ActorExecutor.java
+++ lab4/JavaScriptTester/src/main/java/ru/bmstu/javascript/tester/ActorExecutor.java
@@ -1,6 +1,12 @@
 package ru.bmstu.javascript.tester;
 
-public class ActorExecutor {
+import akka.actor.AbstractActor;
+
+public class ActorExecutor extends AbstractActor {
+    @Override
+    public Receive createReceive() {
+        return null;
+    }
 
     //        ScriptEngine engine = new ScriptEngineManager().getEngineByName("nashorn");
 //        engine.eval("var divideFn = function(a,b) { return a/b}");
