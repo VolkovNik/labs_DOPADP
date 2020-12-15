7e1ae80d15cfdbbd0de75aa5fddbad95fe7219bf
Nikita Volkov <nikitavolkov1707@gmail.com>
2020-12-15 15:40:55 +0300

    Scripted auto-commit on change (2020-12-15 15:40:55) by gitwatch.sh

--- lab4/JavaScriptTester/src/main/java/ru/bmstu/javascript/tester/RequestBody.java
+++ lab4/JavaScriptTester/src/main/java/ru/bmstu/javascript/tester/RequestBody.java
@@ -5,22 +5,38 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class RequestBody {
     @JsonProperty("packageId")
-    private String packageId;
+    private final String packageId;
     @JsonProperty("jsScript")
-    private String jsScript;
+    private final String jsScript;
     @JsonProperty("functionName")
-    private String functionName;
+    private final String functionName;
     @JsonProperty("tests")
-    private String tests;
+    private final String tests;
 
     @JsonCreator
-    public RequestBody(@JsonProperty("packageId")
-                           String packageId
-                                   @JsonProperty("jsScript")
-    String jsScript
-    @JsonProperty("functionName")
-    String functionName
-    @JsonProperty("tests")
-    String tests)
+    public RequestBody(@JsonProperty("packageId") String packageId,
+                       @JsonProperty("jsScript") String jsScript,
+                       @JsonProperty("functionName") String functionName,
+                       @JsonProperty("tests") String tests) {
+        this.packageId = packageId;
+        this.jsScript = jsScript;
+        this.functionName = functionName;
+        this.tests = tests;
+    }
 
+    public String getPackageId() {
+        return packageId;
+    }
+
+    public String getFunctionName() {
+        return functionName;
+    }
+
+    public String getJsScript() {
+        return jsScript;
+    }
+
+    public String getTests() {
+        return tests;
+    }
 }
