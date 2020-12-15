eac798bf38218a1006b32242d529b9d207e7d2ea
Nikita Volkov <nikitavolkov1707@gmail.com>
2020-12-15 14:11:10 +0300

    Scripted auto-commit on change (2020-12-15 14:11:10) by gitwatch.sh

--- lab4/JavaScriptTester/src/main/java/ru/bmstu/javascript/tester/JavaScriptTester.java
+++ lab4/JavaScriptTester/src/main/java/ru/bmstu/javascript/tester/JavaScriptTester.java
@@ -35,8 +35,9 @@ public class JavaScriptTester {
         final CompletionStage<ServerBinding> binding = http.bindAndHandle(
                 routeFlow,
                 ConnectHttp.toHost("localhost", 8080),
-                materializer
-                System.out.println("Server online at http://localhost:8080/\nPress RETURN to stop...");
+                materializer);
+
+        System.out.println("Server online at http://localhost:8080/\nPress RETURN to stop...");
         System.in.read();
         binding
                 .thenCompose(ServerBinding::unbind)
