7db51f40a03cb30d502c9405cd9fc41bd1a47e8e
Nikita Volkov <nikitavolkov1707@gmail.com>
2020-12-15 19:17:59 +0300

    Scripted auto-commit on change (2020-12-15 19:17:59) by gitwatch.sh

--- lab4/JavaScriptTester/src/main/java/ru/bmstu/javascript/tester/ReturnResultMsg.java
+++ lab4/JavaScriptTester/src/main/java/ru/bmstu/javascript/tester/ReturnResultMsg.java
@@ -2,5 +2,18 @@ package ru.bmstu.javascript.tester;
 
 public class ReturnResultMsg {
     private final String packageId;
-    private final String 
+    private final String result;
+
+    public ReturnResultMsg(String packageId, String result) {
+        this.packageId = packageId;
+        this.result = result;
+    }
+
+    public String getPackageId() {
+        return packageId;
+    }
+
+    public String getResult() {
+        return result;
+    }
 }
