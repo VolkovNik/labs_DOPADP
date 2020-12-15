e3d032eac17c476b1e96918555b76f84d8974264
Nikita Volkov <nikitavolkov1707@gmail.com>
2020-12-15 18:02:18 +0300

    Scripted auto-commit on change (2020-12-15 18:02:18) by gitwatch.sh

--- lab4/JavaScriptTester/src/main/java/ru/bmstu/javascript/tester/ActorRouter.java
+++ lab4/JavaScriptTester/src/main/java/ru/bmstu/javascript/tester/ActorRouter.java
@@ -16,6 +16,6 @@ public class ActorRouter extends AbstractActor {
     @Override
     public Receive createReceive() {
         return ReceiveBuilder.create()
-                .match()
+                .match(TestDataMsg)
     }
 }
