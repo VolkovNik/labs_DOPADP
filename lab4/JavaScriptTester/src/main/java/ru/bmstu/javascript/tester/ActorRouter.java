ec17950bc85ded147274b795c6a71d7a7433ef19
Nikita Volkov <nikitavolkov1707@gmail.com>
2020-12-15 17:59:09 +0300

    Scripted auto-commit on change (2020-12-15 17:59:08) by gitwatch.sh

--- lab4/JavaScriptTester/src/main/java/ru/bmstu/javascript/tester/ActorRouter.java
+++ lab4/JavaScriptTester/src/main/java/ru/bmstu/javascript/tester/ActorRouter.java
@@ -1,4 +1,19 @@
 package ru.bmstu.javascript.tester;
 
-public class ActorRouter {
+import akka.actor.AbstractActor;
+import akka.actor.ActorRef;
+import akka.actor.Props;
+import akka.routing.RoundRobinPool;
+
+public class ActorRouter extends AbstractActor {
+
+    private final ActorRef actorExecutor = getContext().actorOf(
+            new RoundRobinPool(5).props(Props.create(ActorExecutor.class)));
+    private final ActorRef actorStorage = getContext().actorOf(Props.create(ActorStorage.class));
+
+
+    @Override
+    public Receive createReceive() {
+        return null;
+    }
 }
