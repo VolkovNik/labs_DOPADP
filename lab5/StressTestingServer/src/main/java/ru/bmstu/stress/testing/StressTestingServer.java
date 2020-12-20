34f0ca59adf52f137d6933a65e062e40bd8208c2
Nikita Volkov <nikitavolkov1707@gmail.com>
2020-12-20 18:15:46 +0300

    Scripted auto-commit on change (2020-12-20 18:15:46) by gitwatch.sh

--- lab5/StressTestingServer/src/main/java/ru/bmstu/stress/testing/StressTestingServer.java
+++ lab5/StressTestingServer/src/main/java/ru/bmstu/stress/testing/StressTestingServer.java
@@ -1,4 +1,58 @@
 package ru.bmstu.stress.testing;
 
-public class StressTestingServer {
+import akka.NotUsed;
+import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
+import akka.actor.Props;
+import akka.http.javadsl.ConnectHttp;
+import akka.http.javadsl.Http;
+import akka.http.javadsl.ServerBinding;
+import akka.http.javadsl.marshallers.jackson.Jackson;
+import akka.http.javadsl.model.HttpRequest;
+import akka.http.javadsl.model.HttpResponse;
+import akka.http.javadsl.server.AllDirectives;
+import akka.http.javadsl.server.Route;
+import akka.pattern.Patterns;
+import akka.stream.ActorMaterializer;
+import akka.stream.javadsl.Flow;
+import scala.concurrent.Future;
+
+import java.io.IOException;
+import java.util.concurrent.CompletionStage;
+
+
+public class StressTestingServer extends AllDirectives {
+
+    private final static String GET_PARAMETER = "packageId";
+    private final static String MSG_TEST_ACCEPT = "Test Accepted \n";
+    private final static int TIMEOUT_FOR_FUTURE = 5;
+    private final static String SYSTEM_NAME = "routes";
+    private final static String HOST = "localhost";
+    private final static int PORT = 8080;
+    private final static String MSG_RUNNING_SERVER = "Server online at http://localhost:8080/\nPress RETURN to stop...";
+
+
+    public static void main(String[] args) throws IOException {
+
+        ActorSystem system = ActorSystem.create(SYSTEM_NAME);
+        final Http http = Http.get(system);
+
+        final ActorMaterializer materializer = ActorMaterializer.create(system);
+        StressTestingServer instance = new StressTestingServer();
+        //ActorRef router  = system.actorOf(Props.create(ActorRouter.class));
+        final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow =
+                instance.createRoute(router).flow(system, materializer);
+        final CompletionStage<ServerBinding> binding = http.bindAndHandle(
+                routeFlow,
+                ConnectHttp.toHost(HOST, PORT),
+                materializer);
+
+        System.out.println(MSG_RUNNING_SERVER);
+        System.in.read();
+        binding
+                .thenCompose(ServerBinding::unbind)
+                .thenAccept(unbound -> system.terminate());
+
+    }
+
 }
