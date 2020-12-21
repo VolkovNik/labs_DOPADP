9f31a5423d48205ba4ce8760a847dcb50ecbcc62
Nikita Volkov <nikitavolkov1707@gmail.com>
2020-12-21 14:56:18 +0300

    Scripted auto-commit on change (2020-12-21 14:56:18) by gitwatch.sh

--- lab5/StressTestingServer/src/main/java/ru/bmstu/stress/testing/StressTestingServer.java
+++ lab5/StressTestingServer/src/main/java/ru/bmstu/stress/testing/StressTestingServer.java
@@ -60,7 +60,7 @@ public class StressTestingServer extends AllDirectives {
                         (req) -> {
                             //System.out.println(req.getUri().query().get("packageId").get());
                             Query queryParams = req.getUri().query();
-                            System.out.println(req.entity());
+                            System.out.println(req.entity().toString());
                             String URL = queryParams.get("testUrl").get();
                             Integer count = Integer.parseInt(queryParams.get("count").get());
                             return new Pair<>(URL, count);
