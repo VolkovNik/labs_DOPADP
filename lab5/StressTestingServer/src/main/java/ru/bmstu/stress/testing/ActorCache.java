3a5e69f9eb4325a3cbf9be9275715840e459d559
Nikita Volkov <nikitavolkov1707@gmail.com>
2020-12-21 16:15:52 +0300

    Scripted auto-commit on change (2020-12-21 16:15:52) by gitwatch.sh

--- lab5/StressTestingServer/src/main/java/ru/bmstu/stress/testing/ActorCache.java
+++ lab5/StressTestingServer/src/main/java/ru/bmstu/stress/testing/ActorCache.java
@@ -1,12 +1,15 @@
 package ru.bmstu.stress.testing;
 
 import akka.actor.AbstractActor;
+import akka.actor.ActorRef;
 import akka.japi.pf.ReceiveBuilder;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 
 public class ActorCache extends AbstractActor {
+    public final Integer CODE_NOT_FOUND = -1;
+
     private final HashMap<String, Integer> testResults = new HashMap<>();
 
     @Override
@@ -15,6 +18,11 @@ public class ActorCache extends AbstractActor {
                 .match(FindResultMsg.class, msg -> {
                     String testUrl = msg.getTestUrl();
                     Integer result = testResults.get(testUrl);
+                    if (result != null) {
+                        getSender().tell(result, ActorRef.noSender());
+                    } else {
+                        getSender().tell(CODE_NOT_FOUND, ActorRef.noSender());
+                    }
                 })
                 .build();
     }
--- lab5/StressTestingServer/src/main/java/ru/bmstu/stress/testing/ReturnResultMsg.java
+++ lab5/StressTestingServer/src/main/java/ru/bmstu/stress/testing/ReturnResultMsg.java
@@ -1,4 +1,21 @@
 package ru.bmstu.stress.testing;
 
 public class ReturnResultMsg {
+    private final String testUrl;
+    private final Integer result;
+
+    public ReturnResultMsg(String testUrl, Integer result) {
+        this.testUrl = testUrl;
+        this.result = result;
+    }
+
+    public Integer getResult() {
+        return result;
+    }
+
+    public String getTestUrl() {
+        return testUrl;
+    }
 }
+
+
